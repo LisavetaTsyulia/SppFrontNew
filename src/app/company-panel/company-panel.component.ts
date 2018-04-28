1345662a08ad98aa68eb581d449b4fda2af01710
LisavetaTsyulia <tsyulialisa@gmail.com>
2018-04-28 07:42:15 +0300

    company panel is working

--- src/app/company-panel/company-panel.component.ts
+++ src/app/company-panel/company-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'app-company-panel',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyPanelComponent implements OnInit {
 
-  constructor() { }
+  http = null;
+  format = 'PDF';
+
+  constructor(http: Http) {
+    this.http = http;
+  }
 
   ngOnInit() {
   }
 
+  printListOfStorages() {
+    const url = 'http://localhost:4321/document/printListOfStorages/' + this.format;
+    window.open(url);
+  }
+
+  printListOfItems() {
+    const url = 'http://localhost:4321/document/printListOfItems/' + this.format;
+    window.open(url);
+  }
 }
